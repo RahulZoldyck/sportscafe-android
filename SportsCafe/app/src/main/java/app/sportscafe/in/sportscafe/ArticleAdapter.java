a66aa8cad76380c69c37c45647f89ff825fc461b
rbkrishna <krishnarb3@gmail.com>
2015-12-02 16:38:23 +0530

    Article Authorname

--- SportsCafe/app/src/main/java/app/sportscafe/in/sportscafe/ArticleAdapter.java
+++ SportsCafe/app/src/main/java/app/sportscafe/in/sportscafe/ArticleAdapter.java
@@ -19,7 +19,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
 {
     Context context;
     ArrayList<Article> articles = new ArrayList<>();
-
+    public static String LOGGING="LOGGING";
     public ArticleAdapter(ArrayList<Article> articles_array,Context context)
     {
         this.articles = articles_array;
@@ -31,6 +31,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         TextView textView_title;
         TextView textView_summary;
         TextView textView_author;
+        TextView textView_sport;
         ImageView image;
         public ViewHolder(View V)
         {
@@ -38,6 +39,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
             textView_title = (TextView)V.findViewById(R.id.title);
             textView_summary = (TextView)V.findViewById(R.id.summary);
             textView_author = (TextView)V.findViewById(R.id.author);
+            textView_sport = (TextView)V.findViewById(R.id.sport);
             image = (ImageView)V.findViewById(R.id.imageView);
         }
 
@@ -56,6 +58,7 @@ public class ArticleAdapter extends RecyclerView.Adapter<ArticleAdapter.ViewHold
         holder.textView_title.setText(articles.get(position).getTitle());
         //holder.textView_summary.setText(articles.get(position).getSummary());
         holder.textView_author.setText(articles.get(position).getAuthor());
+        holder.textView_sport.setText(articles.get(position).getSport().toUpperCase());
         Picasso.with(context)
                 .load(articles.get(position).getImage_URL())
                 .placeholder(R.drawable.sportscafe)
