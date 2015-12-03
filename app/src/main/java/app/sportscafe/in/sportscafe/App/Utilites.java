d45d6cdde167e7e456c7bd3e5dd265c9a55e54fe
rbkrishna <krishnarb3@gmail.com>
2015-12-03 17:32:33 +0530

    Merging Picasso Error resolved

--- app/src/main/java/app/sportscafe/in/sportscafe/App/Utilites.java
+++ app/src/main/java/app/sportscafe/in/sportscafe/App/Utilites.java
@@ -5,19 +5,12 @@ package app.sportscafe.in.sportscafe.App;
  */
 public class Utilites
 {
-<<<<<<< HEAD
-    private String ArticlesWithConditionsURL = "https://sportscafe.in/api/articles/getArticlesWithConditions";
-    private String ArticlesImageURL = "https://sportscafe.in/img/es3";
-    private String FixturesURL ="https://sportscafe.in/api/fixtures/getMatchesWithAggregation";
 
-    public String getFixtureURL() {
-=======
     private static String ArticlesWithConditionsURL = "https://sportscafe.in/api/articles/getArticlesWithConditions";
     private static String ArticlesImageURL = "https://sportscafe.in/img/es3";
     private static String FixturesURL ="https://sportscafe.in/api/fixtures/getMatchesWithAggregation";
 
     public static String getFixtureURL() {
->>>>>>> FETCH_HEAD
         return FixturesURL;
     }
 
