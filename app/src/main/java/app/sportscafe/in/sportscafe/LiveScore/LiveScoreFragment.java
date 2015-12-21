00b0c5307344a689fdc228ba3191d27b42f3b8e0
RahulZoldyck <rahulzoldyck@gmail.com>
2015-12-21 13:35:05 +0530

    Live Scores Started

--- app/src/main/java/app/sportscafe/in/sportscafe/LiveScore/LiveScoreFragment.java
+++ app/src/main/java/app/sportscafe/in/sportscafe/LiveScore/LiveScoreFragment.java
@@ -10,36 +10,18 @@ import android.view.ViewGroup;
 
 import app.sportscafe.in.sportscafe.R;
 
-/**
- * A simple {@link Fragment} subclass.
- * Activities that contain this fragment must implement the
- * {@link LiveScoreFragment.OnFragmentInteractionListener} interface
- * to handle interaction events.
- * Use the {@link LiveScoreFragment#newInstance} factory method to
- * create an instance of this fragment.
- *
- */
 public class LiveScoreFragment extends Fragment {
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
-    // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
 
     private OnFragmentInteractionListener mListener;
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment LiveScoreFragment.
-     */
-    // TODO: Rename and change types and number of parameters
+    public LiveScoreFragment() {
+    }
+
     public static LiveScoreFragment newInstance(String param1, String param2) {
         LiveScoreFragment fragment = new LiveScoreFragment();
         Bundle args = new Bundle();
@@ -47,9 +29,6 @@ public class LiveScoreFragment extends Fragment {
         args.putString(ARG_PARAM2, param2);
         fragment.setArguments(args);
         return fragment;
-    }
-    public LiveScoreFragment() {
-        // Required empty public constructor
     }
 
     @Override
@@ -97,7 +76,7 @@ public class LiveScoreFragment extends Fragment {
      * fragment to allow an interaction in this fragment to be communicated
      * to the activity and potentially other fragments contained in that
      * activity.
-     * <p>
+     * <p/>
      * See the Android Training lesson <a href=
      * "http://developer.android.com/training/basics/fragments/communicating.html"
      * >Communicating with Other Fragments</a> for more information.
