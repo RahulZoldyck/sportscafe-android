45b279e08a54d81294f630dca264a4c901a920f8
RahulZoldyck <rahulzoldyck@gmail.com>
2015-12-24 13:29:29 +0530

    Feeds almost Working

--- app/src/main/java/app/sportscafe/in/sportscafe/App/Utilites.java
+++ app/src/main/java/app/sportscafe/in/sportscafe/App/Utilites.java
@@ -1,25 +1,25 @@
 package app.sportscafe.in.sportscafe.App;
 
-/**
- * Created by rb on 30/11/15.
- */
-public class Utilites
-{
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+public class Utilites {
 
     public static final String ARTICLE_TYPE_MVWEEK = "mvweek";
     public static final String ARTICLE_TYPE_MVMONTH = "mvmonth";
     public static final String PREFERENCES = "myPref";
     public static final String GAME_PREF_ID = "gamePref";
-    private static String articlesWithConditionsURL = "https://sportscafe.in/api/articles/getArticlesWithConditions";
-    private static String initialImageURL = "https://sportscafe.in/img/es3";
-    private static String fixturesURL ="https://sportscafe.in/api/fixtures/getMatchesWithAggregation";
-    private static String teamImg ="https://sportscafe.in/img/es3-cfit-w300-h300/scweb/scapp/partials/images/sports";
-    private static String articleContentURL = "https://sportscafe.in/api/articles/getArticleById/";
+    public static final String image_width = "800";
+    public static final String ARTICLE_TYPE_MVDAY = "mvday";
+    public static final String image_height = "400";
     private static final String stateArticles = "state_articles";
     private static final String TAG = "LOGGING";
-    public static final String image_width = "800";
-    public static final String ARTICLE_TYPE_MVDAY="mvday";
-    public static final String image_height = "400";
+    private static String articlesWithConditionsURL = "https://sportscafe.in/api/articles/getArticlesWithConditions";
+    private static String initialImageURL = "https://sportscafe.in/img/es3";
+    private static String fixturesURL = "https://sportscafe.in/api/fixtures/getMatchesWithAggregation";
+    private static String teamImg = "https://sportscafe.in/img/es3-cfit-w300-h300/scweb/scapp/partials/images/sports";
+    private static String articleContentURL = "https://sportscafe.in/api/articles/getArticleById/";
 
     public static String getTeamImg() {
         return teamImg;
@@ -29,28 +29,42 @@ public class Utilites
         return fixturesURL;
     }
 
-    public static String getArticlesWithConditionsURL()
-    {
+    public static String getArticlesWithConditionsURL() {
         return articlesWithConditionsURL;
     }
 
-    public static String getInitialImageURL(String width,String height,String quality,String articleImageURL)
-    {
-        return initialImageURL+"-cfill-w"+width+"-h"+height+"-qn"+quality+"/"+articleImageURL;
+    public static String getInitialImageURL(String width, String height, String quality, String articleImageURL) {
+        return initialImageURL + "-cfill-w" + width + "-h" + height + "-qn" + quality + "/" + articleImageURL;
     }
 
-    public static String getArticleContentURL()
-    {
+    public static String getArticleContentURL() {
         return articleContentURL;
     }
 
-    public static String getStateArticles()
-    {
+    public static String getStateArticles() {
         return stateArticles;
     }
 
-    public static String getTAG()
-    {
+    public static String getTAG() {
         return TAG;
+    }
+
+    public static String getQuery(String... pref) {
+        JSONArray or = new JSONArray();
+        JSONObject obj;
+        for (String sport : pref) {
+            obj = new JSONObject();
+            try {
+                obj.put("classifications.sections.sport", sport);
+                or.put(obj);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+        return "{\"msg\":{\"conditions\":{\"published\":true," +
+                "\"classifications.sections.articleType\":\"match report\",\"$or\":" + or.toString() + "}," +
+                "\"projection\":{\"content\":0},\"options\":{\"sort\":{\"publishDate\":-1},\"skip\":0," +
+                "\"limit\":8}}}";
+
     }
 }
