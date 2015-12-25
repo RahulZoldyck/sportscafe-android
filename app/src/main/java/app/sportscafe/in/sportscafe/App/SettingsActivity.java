de3d8cee30bbaa3233a178d620a28d7497062274
RahulZoldyck <rahulzoldyck@gmail.com>
2015-12-25 17:57:12 +0530

    Settings Over checking for Bugs

--- app/src/main/java/app/sportscafe/in/sportscafe/App/SettingsActivity.java
+++ app/src/main/java/app/sportscafe/in/sportscafe/App/SettingsActivity.java
@@ -1,173 +1,82 @@
 package app.sportscafe.in.sportscafe.App;
 
-
-import android.annotation.TargetApi;
-import android.app.Activity;
 import android.content.Context;
-import android.content.Intent;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
-import android.content.res.Configuration;
-import android.media.Ringtone;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
 import android.os.Bundle;
-import android.preference.ListPreference;
 import android.preference.MultiSelectListPreference;
-import android.preference.Preference;
-import android.preference.PreferenceActivity;
-import android.support.v7.app.ActionBar;
-import android.preference.PreferenceFragment;
-import android.preference.PreferenceManager;
-import android.preference.RingtonePreference;
-import android.text.TextUtils;
-import android.util.AttributeSet;
-import android.view.MenuItem;
-import android.support.v4.app.NavUtils;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Set;
 
 import app.sportscafe.in.sportscafe.R;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.Iterator;
-import java.util.List;
-import java.util.Set;
-
-public class SettingsActivity extends AppCompatPreferenceActivity {
-
-    private static boolean isXLargeTablet(Context context) {
-        return (context.getResources().getConfiguration().screenLayout
-                & Configuration.SCREENLAYOUT_SIZE_MASK) >= Configuration.SCREENLAYOUT_SIZE_XLARGE;
-    }
-
-
-    private static void bindPreferenceSummaryToValue(Preference preference) {
-        // Set the listener to watch for value changes.
-//        preference.setOnPreferenceChangeListener(sBindPreferenceSummaryToValueListener);
-//
-//        // Trigger the listener immediately with the preference's
-//        // current value.
-//        sBindPreferenceSummaryToValueListener.onPreferenceChange(preference,
-//                PreferenceManager
-//                        .getDefaultSharedPreferences(preference.getContext())
-//                        .getString(preference.getKey(), ""));
-    }
+public class SettingsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setupActionBar();
+        setContentView(R.layout.activity_settings);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
     }
 
-    /**
-     * Set up the {@link android.app.ActionBar}, if the API is available.
-     */
-    private void setupActionBar() {
-        ActionBar actionBar = getSupportActionBar();
-        if (actionBar != null) {
-            // Show the Up button in the action bar.
-            actionBar.setDisplayHomeAsUpEnabled(true);
+    public void gamePrefClicked(View view) {
+        final ArrayList itemsSelected = new ArrayList();
+        final SharedPreferences sharedPreferences = getSharedPreferences("gamePref", Context.MODE_PRIVATE);
+        final SharedPreferences.Editor editor=sharedPreferences.edit();
+        Set<String> gameSet = sharedPreferences.getStringSet("key", null);
+        String[] gameArray={};
+        if (gameSet != null) {
+            gameArray = gameSet.toArray(new String[gameSet.size()]);
         }
-    }
+        String[] totalGames = getResources().getStringArray(R.array.game_pref);
+        boolean[] checkedList=new boolean[totalGames.length];
+        boolean isPresent;
+        int i =0;
+        for(String game : totalGames){
+            isPresent=false;
+            for(String checkedGame : gameArray){
+                if(checkedGame.equals(game))
+                    isPresent=true;
 
-    @Override
-    public boolean onMenuItemSelected(int featureId, MenuItem item) {
-        int id = item.getItemId();
-        if (id == android.R.id.home) {
-            if (!super.onMenuItemSelected(featureId, item)) {
-                NavUtils.navigateUpFromSameTask(this);
             }
-            return true;
+            checkedList[i]=isPresent;
+
         }
-        return super.onMenuItemSelected(featureId, item);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    public boolean onIsMultiPane() {
-        return isXLargeTablet(this);
-    }
-
-    /**
-     * {@inheritDoc}
-     */
-    @Override
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public void onBuildHeaders(List<Header> target) {
-        loadHeadersFromResource(R.xml.pref_headers, target);
-    }
-
-    /**
-     * This method stops fragment injection in malicious applications.
-     * Make sure to deny any unknown fragments here.
-     */
-    protected boolean isValidFragment(String fragmentName) {
-        return PreferenceFragment.class.getName().equals(fragmentName)
-                || GeneralPreferenceFragment.class.getName().equals(fragmentName);
-    }
-
-    /**
-     * This fragment shows general preferences only. It is used when the
-     * activity is showing a two-pane settings UI.
-     */
-    @TargetApi(Build.VERSION_CODES.HONEYCOMB)
-    public static class GeneralPreferenceFragment extends PreferenceFragment {
-        @Override
-        public void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            addPreferencesFromResource(R.xml.pref_general);
-            setHasOptionsMenu(true);
-
-            // Bind the summaries of EditText/List/Dialog/Ringtone preferences
-            // to their values. When their values change, their summaries are
-            // updated to reflect the new value, per the Android Design
-            // guidelines.
-
-
-             final MultiSelectListPreference gamePref = (MultiSelectListPreference) findPreference("multiGameList");
-            String[] strings={"Cricket","Hockey"};
-            Set<String> mySet = new HashSet<String>();
-            Collections.addAll(mySet, strings);
-            gamePref.setValues(mySet);
-            gamePref.setOnPreferenceChangeListener(
-            new Preference.OnPreferenceChangeListener() {
-                        @Override
-                        public boolean onPreferenceChange(Preference preference, Object newValue) {
-                            HashSet hashSet = (HashSet) newValue;
-                            Iterator stringIterator = hashSet.iterator();
-                            String prefString;
-                            ArrayList<String> prefs=new ArrayList<>();
-                            String summary="";
-
-                            while (stringIterator.hasNext()) {
-                                prefString = (String) stringIterator.next();
-                                prefs.add(prefString);
-                                summary+=prefString+" ";
-                            }
-                            Set<String> mySet = new HashSet<String>();
-                            String[] a=new String[prefs.size()];
-                            a=prefs.toArray(a);
-                            Collections.addAll(mySet, a);
-                            gamePref.setValues(mySet);
-                            return false;
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Game Preference");
+        builder.setMultiChoiceItems(totalGames, checkedList,
+                new DialogInterface.OnMultiChoiceClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int selectedItemId,
+                                        boolean isSelected) {
+                            if (isSelected) {
+                            itemsSelected.add(selectedItemId);
+                        } else if (itemsSelected.contains(selectedItemId)) {
+                            itemsSelected.remove(Integer.valueOf(selectedItemId));
                         }
                     }
-            );
-            bindPreferenceSummaryToValue(findPreference("multiGameList"));
-
-        }
-
-        @Override
-        public boolean onOptionsItemSelected(MenuItem item) {
-            int id = item.getItemId();
-            if (id == android.R.id.home) {
-                startActivity(new Intent(getActivity(), SettingsActivity.class));
-                return true;
-            }
-            return super.onOptionsItemSelected(item);
-        }
+                })
+                .setPositiveButton("Done!", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                        Set<String> itemSelected = new HashSet<String>(itemsSelected);
+                        editor.putStringSet("key", itemSelected);
+                        editor.apply();
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int id) {
+                    }
+                });
     }
 }
